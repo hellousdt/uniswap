7e221cd846e7d3a8ba69de2a4e72affdb023676d
hellousdt <hellousdt@gmail.com>
2021-04-15 18:03:25 +0800

    modify ReentrancyGuard

--- contracts/DFAM.sol
+++ contracts/DFAM.sol
@@ -120,17 +120,19 @@ library SafeERC20 {
 }
 
 contract ReentrancyGuard {
-    uint256 private _guardCounter;
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+    uint256 private _status;
 
     constructor () internal {
-        _guardCounter = 1;
+        _status = _NOT_ENTERED;
     }
 
     modifier nonReentrant() {
-        _guardCounter += 1;
-        uint256 localCounter = _guardCounter;
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
         _;
-        require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
+        _status = _NOT_ENTERED;
     }
 }
 
